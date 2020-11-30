f22c6c59d2fb5e084a9d2d63254a03b27e8ec833
Programmercito <osboceo@live.com>
2020-11-29 23:26:20 -0400

    cambios para rest

--- src/main/java/bo/digital/test/web/controllers/HospitalesWeb.java
+++ src/main/java/bo/digital/test/web/controllers/HospitalesWeb.java
@@ -7,9 +7,13 @@ package bo.digital.test.web.controllers;
 
 import bo.digital.test.model.entities.SisHospitales;
 import bo.digital.test.model.interfaces.services.IHospitalesService;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +32,19 @@ public class HospitalesWeb {
     @GetMapping("/hospitales")
     public List<SisHospitales> findAll() {
         return hospitales.listar();
+    }
+
+    @PostMapping("/hospitales")
+    public SisHospitales add(SisHospitales hospital) {
+        return hospitales.save(hospital);
+    }
+
+    @DeleteMapping("/hospitales")
+    public Map<String, String> delete(SisHospitales hospital) {
+        Map<String, String> resul = new HashMap< String, String>();
+        hospitales.delete(hospital);
+        resul.put("resultado", "ok");
+        return resul;
     }
 
 }
